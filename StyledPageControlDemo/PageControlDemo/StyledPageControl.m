a7dec3a1bf96d8d6ddd9e2e6fa3be7b2acc251ca
Sudeep Jaiswal <sudeep.jaiswal87@gmail.com>
2015-02-12 15:18:18 +0530

    [Fix] warnings from break statements

--- StyledPageControlDemo/PageControlDemo/StyledPageControl.m
+++ StyledPageControlDemo/PageControlDemo/StyledPageControl.m
@@ -199,15 +199,15 @@
 				
 				if (gap==2)
 				{
+                    total_width = self.numberOfPages*diameter + (self.numberOfPages-1)*gap;
 					break;
-					total_width = self.numberOfPages*diameter + (self.numberOfPages-1)*gap;
 				}
 			}
 			
 			if (diameter==2)
 			{
-				break;
-				total_width = self.numberOfPages*diameter + (self.numberOfPages-1)*gap;
+                total_width = self.numberOfPages*diameter + (self.numberOfPages-1)*gap;
+                break;
 			}
 		}
 		
